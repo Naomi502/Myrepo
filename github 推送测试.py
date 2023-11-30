62654fafa702c20e0da0b406bc1d68a7d743c945
Ocrekta <Oikta34@outlook.com>
2023-11-30 14:05:04 +0800

    '自动化提交'

--- github 推送测试.py
+++ github 推送测试.py
@@ -1,14 +1,17 @@
-ifpush = True
 import os
-import sys
 
-def push():
-    if ifpush:
-        os.system("git add .")
-        os.system("git commit -m '自动化测试'")
-        os.system("git push")
-        print("推送成功")
-    else:
-        sys.exit()
+def push_to_remote():
+    # 检查当前目录是否为一个 Git 仓库
+    if not os.path.exists(".git"):
+        print("当前目录不是一个 Git 仓库，请进入正确的目录。")
+        return
 
-push()
+    # 执行 Git 操作
+    os.system("git add .")
+    os.system("git commit -m '自动化提交'")
+    os.system("git push")
+
+    print("本地代码成功推送到远程仓库。")
+
+# 执行推送操作
+push_to_remote()
