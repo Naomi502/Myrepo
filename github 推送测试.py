56e76e37d77640b92f26dab0abad6cc8bc08836e
Ocrekta <Oikta34@outlook.com>
2023-11-30 14:04:08 +0800

    '自动化测试'

--- github 推送测试.py
+++ github 推送测试.py
@@ -1,8 +1,10 @@
 ifpush = True
 import os
+import sys
+
 def push():
     if ifpush:
-        os.system("git add.")
+        os.system("git add .")
         os.system("git commit -m '自动化测试'")
         os.system("git push")
         print("推送成功")
