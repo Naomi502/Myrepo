7f43d0b224651b687170da90c8130cb89c4fa12a
Ocrekta <Oikta34@outlook.com>
2023-12-01 15:22:57 +0800

    '本次自动化提交成功_code34'

---
+++ new.py
@@ -0,0 +1,13 @@
+x = eval(input())
+if x < 60:
+    print("不及格")
+elif x < 70:
+    print("及格")
+elif x < 80:
+    print("中等")
+elif x < 90:
+    print("良好")
+elif x < 100:
+    print("优秀")
+else:
+    print("输入有误")
